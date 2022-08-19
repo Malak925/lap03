3c54ab751592b21bfe80c6afb51e74d07025f926
Malak925 <Malakdawod925@gmail.com>
2022-08-19 18:30:35 +0300

    second attempt

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -16,7 +16,6 @@ class My_app extends StatelessWidget {
         title: Text("Malak.D"),
       ),
       drawer: Drawer(
-        
         backgroundColor: Colors.pinkAccent,
       ),
       body: Container(
@@ -24,44 +23,82 @@ class My_app extends StatelessWidget {
           Card(
             child: Image(
               image: NetworkImage(
-                  "https://ih1.redbubble.net/image.749737322.4435/st,small,845x845-pad,1000x1000,f8f8f8.u4.jpg"),
+                  "https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"),
+              width: double.infinity,
+              height: 250,
             ),
           ),
           Card(
-            child: Row( children: [
-
-              Container(
-             
-                decoration: BoxDecoration(border: Border.all(width: 4.0,color: Colors.deepPurple),),
-
-                child: Text("kitty"),
-              
-
-
-
-              ),
-              Container(
-                decoration: BoxDecoration(border: Border.all(width: 4.0,color: Colors.blueGrey),),
-
-                child: Text("female"),
-              ),
-              Container(
-                decoration: BoxDecoration(border: Border.all(width: 4.0,color: Colors.blueAccent),),
-
-                child: Text("6 months"),
-              ),
-
-
-
-
-
-            ],
-
-
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: (LinearGradient(
+                        colors: [Colors.pink, Colors.yellowAccent])),
+                    border: Border.all(width: 4.0, color: Colors.deepPurple),
+                  ),
+                  child: Text("kitty"),
+                ),
+                SizedBox(width: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    border: Border.all(width: 4.0, color: Colors.blueGrey),
+                  ),
+                  child: Text("female"),
+                ),
+                SizedBox(width: 100),
+                Container(
+                  decoration: BoxDecoration(
+                   color: Colors.yellowAccent,
+                    border: Border.all(width: 4.0, color: Colors.blueAccent),
+                  ),
+                  child: Text("6 months"),
+                ),
+              ],
             ),
           ),
-          Card(),
-          Card()
+          Card(
+            child: Image(
+              image: NetworkImage(
+                  "https://ichef.bbci.co.uk/news/976/cpsprodpb/C17B/production/_126313594_gettyimages-1217576289.jpg"),
+              width: 150,
+              height: 250,
+            ),
+          ),
+          Card(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                   
+                        color: Colors.pink,
+                    border: Border.all(width: 4.0, color: Colors.deepPurple),
+                  ),
+                  child: Text("kitty"),
+                ),
+                SizedBox(width: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: (LinearGradient(
+                        colors: [Colors.grey, Colors.yellowAccent])),
+                    border: Border.all(width: 4.0, color: Colors.blueGrey),
+                  ),
+                  child: Text("female")
+                ),
+                SizedBox(width: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    border: Border.all(width: 4.0, color: Colors.blueAccent),
+                  ),
+                  child: Text("6 months",style:TextStyle(color:Colors.green)),
+                ),
+              ],
+            ),
+          )
         ]),
       ),
     ));
